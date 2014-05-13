7538c4878a3888d5ff1282475160f2854c41bf78
John <johnmarksjr@gmail.com>
2014-05-13 15:11:42 -0600

    Added Topic Add form to topic panel
    Refactored JS code to use module pattern

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -7,125 +7,52 @@ Utils.numberWithCommas = function(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-// Using a getJSON AJAX request
-function postSnippet(form) {
-    var data = $("#snippetForm").serialize();
-    console.log('The snippet title = ' + title);
-    console.log('The snippet description = ' + description);
-    console.log('The snippet code = ' + code);
-    console.log('The data = ' + data);
-    /*$.getJSON('/snippet', function(data) {*/
-    $.post('/snippet', data, function(data) {
-        console.log("Received an AJAX return from the server");
-        //$("#result").text(data.result);
-    });
-    return false;
-}
-
-$(document).ready(function() {
-
+var snippet = (function() {
     var SNIPPET_HORIZONTAL = 'snippetDes-horz',
         SNIPPET_VERTICAL = 'snippetDes-vert',
+        SNIPPET_TITLESONLY = 'snippetDes-vert',
         snippetDesLayout = SNIPPET_HORIZONTAL,
-        snippetCodeLayout = SNIPPET_HORIZONTAL,
-        topicPanelWidthRatio = topicPanelRatio();
+        snippetCodeLayout = SNIPPET_HORIZONTAL;
 
-    function topicPanelRatio() {
-        return $('#topicPanel').width() / $('#topicPanel').parent().width();
+    /*
+     * Local methods
+     */
+
+    var saveTopic = function(form) {
+        console.log("Saving a new topic");
+        return false;
     }
 
-    $('#snippetAdd').click(function() {
-        $('#snippetForm').show();
-    });
-
-    $('#snippetCancel').click(function() {
-        $('#snippetForm').hide();
-        $('#snippetForm')[0].reset();
-    });
-
-    // Use AJAX to GET a list snippets
-    // Called when a topic in the topic panel is clicked
-    $('#topicPanel div.panel-body li').click(function() {
-        /*
-         * Displays the selected topic in the snippet panel,
-         * sends an AJAX 'GET' request to get the list of snippets
-         * associated with the topic, and updates the snippet panel
-         * with the returned snippets
-         */
-
-        var that = $(this),
-            // Get only the "li a" element text, not the children span's badge "count" text
-            topicname = $(this).find('a').clone().children().remove().end().text();
-        if (!topicname) return;
-
-        var ajaxOptions = {
-            url:'snippets/' + topicname,
-            type: 'GET',
-            dataType: "json",
-            success: function(results) {
-                var count = updateTopicSnippets(results);
-                console.log("AJAX returned with a list of snippets");
-
-                // Update the UI to show the currently displayed topic snippets
-                $('#snippetTopicSearchDisplay').text(topicname);
-
-                // Select topic in topic panel
-                $('#topicPanel div.panel-body li').removeClass('active');
-                that.addClass('active');
-                that.find('a span').text(count);
-            },
-            error: function(req, status, error) {
-                console.log("AJAX returned with error");
+    var buildSnippet = function(title, description, code) {
+        var ss  = '<div class="snippet">';
+        ss +=     '    <span class="snippetID" style="display:none">snippet_id</span>';
+        ss +=     '    <div class="snippetTitle">';
+        ss +=     '        <h5>' + title + '</h5>';
+        ss +=     '        <div class="snippetContent">';
+        if (description) {
+            if (snippetDesLayout != SNIPPET_TITLESONLY) {
+                ss += '            <div class="' + snippetDesLayout + '">';
             }
-        };
-
-        $.ajax(ajaxOptions);
-    });
-
-    // Use AJAX to POST the new snippet
-    $('#snippetSave').click(function() {
-        /*
-         * Creates a new snippet from the snippet form control,
-         * sends an AJAX request to persist the new snippet,
-         * and adds the new snippet to the DOM
-         */
-
-        var that = $(this),
-            title = $('#titleField').val(),
-            data = $("#snippetForm").serialize(),
-            topicname = $('#snippetTopicSearchDisplay').text();
-
-        // Must have at least a snippet title
-        if (!title) return false;
-
-        // Let the snippet get added to the "General" topic if no topic is current selected.
-        // The user always has the "General" topic
-        if (!topicname) {
-            topicname = 'General'
+            ss += '                <p class="snippetDesStyle">' + description + '</p>';
+            ss += '            </div>';
+        } else {
+            if (snippetDesLayout != SNIPPET_TITLESONLY) {
+                ss += '            <div class="' + snippetDesLayout + '">';
+            }
+            ss += '            </div>';
         }
-
-        var ajaxOptions = {
-            url:'snippets/' + topicname,
-            type: 'POST',
-            dataType: "json",
-            data: data,
-            success: function(results) {
-                displayNewSnippet(results['id']);
-                incrementTopicCount();
-            },
-            error: function(req, status, error) {
-                console.log("AJAX returned with error");
+        if (code) {
+            if (snippetCodeLayout != SNIPPET_TITLESONLY) {
+                ss += '            <div class="' + snippetCodeLayout + '">';
             }
-        };
+            ss += '                <pre class="snippetCodeStyle">' + code + '</pre>';
+            ss += '            </div>';
+        }
+        ss +=     '        </div>';
+        ss +=     '    </div>';
+        ss +=     '</div>';
 
-        $.ajax(ajaxOptions);
-        return false;
-    });
-
-    var incrementTopicCount = function() {
-        var badge = $('#topicPanel .list-group li.active').find('a span');
-            badge_count = Number(badge.text());
-        badge.text(badge_count + 1);
+        return ss;
     }
 
     var displayNewSnippet = function(snippet_id) {
@@ -141,6 +68,12 @@ $(document).ready(function() {
         
         // Create a new snippet with the form data
         $('#userSnippets').prepend(ss);
+    }
+
+    var incrementTopicCount = function() {
+        var badge = $('#topicPanel .list-group li.active').find('a span');
+            badge_count = Number(badge.text());
+        badge.text(badge_count + 1);
     }
 
     var updateTopicSnippets = function(snippets) {
@@ -165,50 +98,127 @@ $(document).ready(function() {
         return count;
     }
 
-    var buildSnippet = function(title, description, code) {
-        var ss  = '<div class="snippet">';
-        ss +=     '    <span class="snippetID" style="display:none">snippet_id</span>';
-        ss +=     '    <div class="snippetTitle">';
-        ss +=     '        <h5>' + title + '</h5>';
-        ss +=     '        <div class="snippetContent">';
-        if (description) {
-            ss += '            <div class="' + snippetDesLayout + '">';
-            ss += '                <p class="snippetDesStyle">' + description + '</p>';
-            ss += '            </div>';
-        } else {
-            ss += '            <div class="' + snippetDesLayout + '">';
-            ss += '            </div>';
+
+    /*
+     * Public methods
+     */
+
+    var createSnippet = function(snippetAddButton) {
+        /*
+         * Creates a new snippet from the snippet form control,
+         * sends an AJAX request to persist the new snippet,
+         * and adds the new snippet to the DOM
+         */
+
+        var that = $(snippetAddButton),
+            title = $('#titleField').val(),
+            data = $("#snippetForm").serialize(),
+            topicname = $('#snippetTopicSearchDisplay').text();
+
+        // Must have at least a snippet title
+        if (!title) return false;
+
+        // Let the snippet get added to the "General" topic if no topic is current selected.
+        // The user always has the "General" topic
+        if (!topicname) {
+            topicname = 'General'
         }
-        if (code) {
-            ss += '            <div class="' + snippetCodeLayout + '">';
-            ss += '                <pre class="snippetCodeStyle">' + code + '</pre>';
-            ss += '            </div>';
-        }
-        ss +=     '        </div>';
-        ss +=     '    </div>';
-        ss +=     '</div>';
 
-        return ss;
-    }
-
-
-    // Eatup the form keyboard 'enter' event, so the user must click the submit button
-    $("#snippetForm").bind("keyup keypress", function(event) {
-        var code = event.keyCode || event.which,
-            target = event.target.nodeName;
-
-        // Don't eat up the 'enter' key when typing in <textarea> tags.
-        if (target.toUpperCase().search('TEXTAREA') == -1) {
-            if (code == 13) {
-                event.preventDefault();
-                return false;
+        // Use AJAX to POST the new snippet
+        var ajaxOptions = {
+            url:'snippets/' + topicname,
+            type: 'POST',
+            dataType: "json",
+            data: data,
+            success: function(results) {
+                displayNewSnippet(results['id']);
+                incrementTopicCount();
+            },
+            error: function(req, status, error) {
+                console.log("AJAX returned with error");
             }
-        }
-    });
+        };
 
+        $.ajax(ajaxOptions);
+        return false;
+    };
+
+    var displayTopicSnippet = function(topicItem) {
+        /*
+         * Displays the selected topic in the snippet panel,
+         * sends an AJAX 'GET' request to get the list of snippets
+         * associated with the topic, and updates the snippet panel
+         * with the returned snippets
+         */
+
+        var that = $(topicItem),
+            // Get only the "li a" element text, not the children span's badge "count" text
+            topicname = $(topicItem).find('a').clone().children().remove().end().text();
+        if (!topicname) return;
+
+        // Use AJAX to GET a list snippets
+        var ajaxOptions = {
+            url:'snippets/' + topicname,
+            type: 'GET',
+            dataType: "json",
+            success: function(results) {
+                var count = updateTopicSnippets(results);
+                console.log("AJAX returned with a list of snippets");
+
+                // Update the UI to show the currently displayed topic snippets
+                $('#snippetTopicSearchDisplay').text(topicname);
+
+                // Select topic in topic panel
+                $('#topicPanel div.panel-body li').removeClass('active');
+                that.addClass('active');
+                that.find('a span').text(count);
+            },
+            error: function(req, status, error) {
+                console.log("AJAX returned with error");
+            }
+        };
+
+        $.ajax(ajaxOptions);
+    };
+
+    var showSnippetsHorizontal = function() {
+        $('.snippetDes-vert').toggleClass('snippetDes-vert snippetDes-horz');
+        $('.snippetCode-vert').toggleClass('snippetCode-vert snippetCode-horz');
+        $('.snippetDes-horz').css('display', 'block');
+        $('.snippetCode-horz').css('display', 'block');
+        $('#snippetHorzIcon span').addClass('active');
+        $('#snippetVertIcon span').removeClass('active');
+        $('#snippetTitleOnlyIcon span').removeClass('active');
+        snippetDesLayout = SNIPPET_HORIZONTAL;
+        snippetCodeLayout = SNIPPET_HORIZONTAL;
+    };
+
+    var showSnippetsVertical = function() {
+        $('.snippetDes-horz').toggleClass('snippetDes-horz snippetDes-vert');
+        $('.snippetCode-horz').toggleClass('snippetCode-horz snippetCode-vert');
+        $('.snippetDes-vert').css('display', 'block');
+        $('.snippetCode-vert').css('display', 'block');
+        $('#snippetHorzIcon span').removeClass('active');
+        $('#snippetVertIcon span').addClass('active');
+        $('#snippetTitleOnlyIcon span').removeClass('active');
+        snippetDesLayout = SNIPPET_VERTICAL;
+        snippetCodeLayout = SNIPPET_VERTICAL;
+    };
+
+    var showSnippetTitlesOnly = function() {
+        $('.snippetDes-vert').css('display', 'none');
+        $('.snippetCode-vert').css('display', 'none');
+        $('.snippetDes-horz').css('display', 'none');
+        $('.snippetCode-horz').css('display', 'none');
+        $('#snippetHorzIcon span').removeClass('active');
+        $('#snippetVertIcon span').removeClass('active');
+        $('#snippetTitleOnlyIcon span').addClass('active');
+        snippetDesLayout = SNIPPET_TITLESONLY;
+        snippetCodeLayout = SNIPPET_TITLESONLY;
+    };
 
     // Use VEX dialogs to show the application instructions
-    function showSigninDialog() {
+    var showSigninDialog = function() {
 		vex.open({
 			contentClassName: 'signinDialog',
 			content: 
@@ -231,49 +241,110 @@ $(document).ready(function() {
 			overlayClassName:'signinDialogOverlay',
 			showCloseButton:false});
 	}
-    $('#signin').click(showSigninDialog);
 
-    $('#snippetHorzIcon').click(function() {
-        $('.snippetDes-vert').toggleClass('snippetDes-vert snippetDes-horz');
-        $('.snippetCode-vert').toggleClass('snippetCode-vert snippetCode-horz');
-        $('.snippetDes-horz').css('display', 'block');
-        $('.snippetCode-horz').css('display', 'block');
-        $('#snippetHorzIcon span').addClass('active');
-        $('#snippetVertIcon span').removeClass('active');
-        $('#snippetTitleOnlyIcon span').removeClass('active');
-        snippetDesLayout = SNIPPET_HORIZONTAL;
-        snippetCodeLayout = SNIPPET_HORIZONTAL;
+    return {
+        saveTopic:saveTopic,
+        createSnippet:createSnippet,
+        displayTopicSnippet:displayTopicSnippet,
+        showSnippetsHorizontal:showSnippetsHorizontal,
+        showSnippetsVertical:showSnippetsVertical,
+        showSnippetTitlesOnly:showSnippetTitlesOnly,
+        showSigninDialog:showSigninDialog
+    };
+})();
+
+
+$(document).ready(function() {
+
+    var topicPanelWidthRatio = topicPanelRatio();
+
+    function topicPanelRatio() {
+        return $('#topicPanel').width() / $('#topicPanel').parent().width();
+    }
+
+    /* 
+     * Header Controls
+     */
+
+    // 'Sign In' link is clicked
+    $('#signin').click(snippet.showSigninDialog);
+
+
+    /* 
+     * Topic Panel Controls
+     */
+
+    // Topic 'add' button is clicked
+    $('#topicAdd').click(function() {
+        $('#topicFormContainer').toggle();
     });
 
-    $('#snippetVertIcon').click(function() {
-        $('.snippetDes-horz').toggleClass('snippetDes-horz snippetDes-vert');
-        $('.snippetCode-horz').toggleClass('snippetCode-horz snippetCode-vert');
-        $('.snippetDes-vert').css('display', 'block');
-        $('.snippetCode-vert').css('display', 'block');
-        $('#snippetHorzIcon span').removeClass('active');
-        $('#snippetVertIcon span').addClass('active');
-        $('#snippetTitleOnlyIcon span').removeClass('active');
-        snippetDesLayout = SNIPPET_VERTICAL;
-        snippetCodeLayout = SNIPPET_VERTICAL;
+    // Topic in topic panel is clicked
+    $('#topicPanel div.panel-body li').click(function() {
+        snippet.displayTopicSnippet(this);
     });
 
-    $('#snippetTitleOnlyIcon').click(function() {
-        $('.snippetDes-vert').css('display', 'none');
-        $('.snippetCode-vert').css('display', 'none');
-        $('.snippetDes-horz').css('display', 'none');
-        $('.snippetCode-horz').css('display', 'none');
-        $('#snippetHorzIcon span').removeClass('active');
-        $('#snippetVertIcon span').removeClass('active');
-        $('#snippetTitleOnlyIcon span').addClass('active');
+
+    /* 
+     * Snippet Panel Controls
+     */
+
+    // 'Topic Toggle' icon in snippet panel is clicked
+    //$('#toggleIcon').click(updateTopicPanel);
+
+    // Snippet 'add' button is clicked
+    $('#snippetAdd').click(function() {
+        $('#snippetForm').show();
     });
 
-    function showTopicPanel(callback) {
+    // 'Columns' icon in snippet panel is clicked
+    $('#snippetHorzIcon').click(snippet.showSnippetsHorizontal);
+
+    // 'Rows' icon in snippet panel is clicked
+    $('#snippetVertIcon').click(snippet.showSnippetsVertical);
+
+    // 'Title Only' icon in snippet panel is clicked
+    $('#snippetTitleOnlyIcon').click(snippet.showSnippetTitlesOnly);
+
+
+    /* 
+     * Snippet Controls
+     */
+
+    // New snippet 'save' button clicked
+    $('#snippetSave').click(function() {
+        snippet.createSnippet(this);
+    });
+
+    // New snippet 'cancel' button clicked
+    $('#snippetCancel').click(function() {
+        $('#snippetForm').hide();
+        $('#snippetForm')[0].reset();
+    });
+
+    // Eatup the form keyboard 'enter' event, so the user must click the submit button
+    $("#snippetForm").bind("keyup keypress", function(event) {
+        var code = event.keyCode || event.which,
+            target = event.target.nodeName;
+
+        // Don't eat up the 'enter' key when typing in <textarea> tags.
+        if (target.toUpperCase().search('TEXTAREA') == -1) {
+            if (code == 13) {
+                event.preventDefault();
+                return false;
+            }
+        }
+    });
+
+
+    var showTopicPanel = function(callback) {
         var topicPanelWidth = $('#topicPanel').width(),
             snippetPanelWidth = $('#snippetPanel').width(),
             snippetBlockWidth = $('#snippetBlock').width(),
             deltaFactor = 0.3, // decrease for faster animation
             delta = snippetBlockWidth / (100.0 * deltaFactor),  // make larger 1/100 of the whole snippet area
             newTPW = topicPanelWidth + delta;
+
         if (newTPW < (snippetBlockWidth * topicPanelWidthRatio)) {
             $('#snippetPanel').width(snippetPanelWidth - delta);
             $('#topicPanel').width(newTPW);
@@ -290,7 +361,7 @@ $(document).ready(function() {
         }
     }
 
-    function hideTopicPanel(callback) {
+    var hideTopicPanel = function(callback) {
         var topicPanelWidth = $('#topicPanel').width(),
             snippetPanelWidth = $('#snippetPanel').width(),
             snippetBlockWidth = $('#snippetBlock').width(),
@@ -311,7 +382,7 @@ $(document).ready(function() {
         }
     }
 
-    $('#toggleIcon').click(function() {
+    var updateTopicPanel = function() {
         if ($(this).hasClass('deactivate')) return;
         if($('#topicPanel').css('display') != 'none') {
             hideTopicPanel(function() {
@@ -323,12 +394,12 @@ $(document).ready(function() {
                 delta = snippetBlockWidth / 100.0;
             $('#snippetPanel').width(snippetPanelWidth - delta/4);
             $("#topicPanel").css('display', 'block');
-            showTopicPanel(function() {
+            showTopicPanel(topicPanelWidthRatio, function() {
                 $('#toggleIcon span').addClass('fa-rotate-90');
             });
         }
-    });
-
+    };
+    $('#toggleIcon').click(updateTopicPanel);
 
 });
 
