6cab3152f78a50b33de01bb06430c0cb535a6d55
John <johnmarksjr@gmail.com>
2014-06-03 16:43:28 -0600

    Updated public count in snippet search for both logged in and logged out user

--- app/views.py
+++ app/views.py
@@ -21,7 +21,8 @@ def before_request():
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', page='index')
+    public_count = Snippet.query.filter_by(access=ACCESS_PUBLIC).count()
+    return render_template('index.html', page='index', public_count=public_count)
 
 
 @app.route('/signin/<social>')
@@ -50,9 +51,10 @@ def user():
     personal_count = 0
     for topic in topics:
         personal_count += topic.snippets.count()
+    public_count = Snippet.query.filter_by(access=ACCESS_PUBLIC).count()
     
     return render_template('user.html', name=g.user.name, topics = topics.all(),
-                           page='home', personal_count=personal_count)
+                           page='home', personal_count=personal_count, public_count=public_count)
 
 
 @app.route('/topic/<atopic>', methods = ['POST', 'PUT', 'DELETE'])
